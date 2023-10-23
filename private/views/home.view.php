231f8a3582accd29ea9cc72880042bea23d27850
Dilravi Dhananja <107805922+dhananja6557@users.noreply.github.com>
2023-10-23 10:06:00 +0530

    Level_2

--- private/views/home.view.php
+++ private/views/home.view.php
@@ -1,16 +1,35 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>Home Page</title>
+<?php $this->view('incdocs/header'); ?>
 
-	<link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">
-	<link rel="stylesheet" type="text/css" href="assets/css/all.min.css">
-</head>
-<body>
-	<h1><i class="fa fa-folder-open"></i>Home Page Design</h1>
+<!-- <h1>
+	<i class="fa fa-folder-open"></i>
+	Home Page Design
+</h1> -->
 
-	<script src="assets/js/bootstrap.min.js"></script>
-</body>
-</html>
+<div class="container">
+	<div class="text-center" data-new-gr-c-s-check-loaded="8.907.0" data-gr-ext-installed="">   
+		<main class="form-signin">
+		  	<form>
+		    	<img class="mb-4" src="assets/img/logo007.webp" alt="" width="72" height="57">
+		    	<h1 class="h3 mb-3 fw-normal">Please sign in</h1>
+
+		    	<div class="form-floating mt-3">
+		      		<input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
+		      		<label for="floatingInput">Email address</label>
+		    	</div>
+			    <div class="form-floating mt-3">
+			      	<input type="password" class="form-control" id="floatingPassword" placeholder="Password">
+			      	<label for="floatingPassword">Password</label>
+			    </div>
+
+			    <div class="checkbox mb-3">
+			      	<label>
+			        	<input type="checkbox" value="remember-me"> Remember me
+			      	</label>
+			    </div>
+		    	<button class="w-100 btn btn-lg btn-primary" type="submit">Sign in</button>
+		    	<p class="mt-5 mb-3 text-muted">© 2017–2021</p>
+		  	</form>
+		</main>
+	</div>
+</div>
+<?php $this->view('incdocs/footer'); ?>
---
+++ private/views/incdocs/header.view.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Home Page</title>
+
+	<link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="assets/css/all.min.css">
+</head>
+<body>
